85cbf37c50faf2e9d8266d68b38eaca522b2b832
igarashi kazuki <sousiki.links@gmail.com>
2022-06-05 19:01:02 +0900

    add: カラーテーマ切り替え用のボタンコンポーネントを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,48 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
-import { MantineProvider } from "@mantine/core";
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+  Box,
+} from "@mantine/core";
+import { useState } from "react";
+import ToggleColorSchemeButton from "src/commons/ToggleColorSchemeButton";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
   return (
-    <MantineProvider
-      theme={{ colorScheme: "dark" }}
-      withGlobalStyles
-      withNormalizeCSS
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
     >
-      <div className="mx-auto max-w-5xl">
-        <header className="border-b border-gray-300 py-6">
-          <h1>
-            <Link href="/">
-              <a className="text-xl font-bold">i_garasi blog</a>
-            </Link>
-          </h1>
-        </header>
-        <main className="mt-6">
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </MantineProvider>
+      <MantineProvider
+        theme={{ colorScheme: colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Box className="mx-auto max-w-5xl">
+          <header className="border-b border-gray-300 py-6">
+            <Box className="flex items-center justify-between ">
+              <h1>
+                <Link href="/">
+                  <a className="text-xl font-bold">i_garasi blog</a>
+                </Link>
+              </h1>
+              <Box>
+                <ToggleColorSchemeButton />
+              </Box>
+            </Box>
+          </header>
+          <main className="mt-6">
+            <Component {...pageProps} />
+          </main>
+        </Box>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
