4eda553ba73db64c2ca364a33d272911649c0839
igarashi kazuki <sousiki.links@gmail.com>
2022-06-05 17:36:50 +0900

    add: MantineProvider & TypographyProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,28 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="mx-auto max-w-prose">
-      <header className="border-b border-gray-300 py-6">
-        <h1>
-          <Link href="/">
-            <a className="text-xl font-bold text-cyan-600">i_garasi blog</a>
-          </Link>
-        </h1>
-      </header>
-      <main className="mt-6">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <MantineProvider
+      theme={{ colorScheme: "dark" }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <div className="mx-auto max-w-5xl">
+        <header className="border-b border-gray-300 py-6">
+          <h1>
+            <Link href="/">
+              <a className="text-xl font-bold">i_garasi blog</a>
+            </Link>
+          </h1>
+        </header>
+        <main className="mt-6">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </MantineProvider>
   );
 }
 
